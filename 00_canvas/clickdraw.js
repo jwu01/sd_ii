cb7a72b529a474362834e21a8601a40c2aba3000
Jeffrey Wu <jefffreywu@gmail.com>
2019-01-30 20:51:51 -0500

    finished k00

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,8 +1,24 @@
-var c = document.getElementbyId("slate");
+var c = document.getElementById("slate");
+var ctx = c.getContext('2d');
+
+var tog = document.getElementById("rekt")
+var clear = document.getElementById("clear")
 
 c.addEventListener('click', function(e) {
-    var ctx = cavas.getContext('2d');
-    x = e.clientX
-    y = e.clientY
-    cont.fillRectangle(x,y,10,10)
+    console.log(e);
+    x = e.offsetX;
+    y = e.offsetY;
+
+    if (tog.checked)
+        ctx.fillRect(x,y,10,15);
+    else
+    {
+        ctx.beginPath();
+        ctx.ellipse(x, y, 5, 5, Math.PI / 4, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+})
+
+clear.addEventListener('click', function (e) {
+    ctx.clearRect(0, 0, c.width, c.height);	
 })
