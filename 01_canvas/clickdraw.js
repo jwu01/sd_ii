47d13f07c97a6a7f31a2632730bd588de72c66ad
Jeffrey Wu <jefffreywu@gmail.com>
2019-01-31 21:19:12 -0500

    added k1

---
+++ 01_canvas/clickdraw.js
@@ -0,0 +1,44 @@
+// Tina Wong, Jeffrey Wu (Team friends)
+// SoftDev2 pd7
+// K01 - ...and I want to Paint It Better
+// 2019-01-31
+
+var c = document.getElementById("slate");
+var width = c.getAttribute("width");
+var height = c.getAttribute("height");
+var ctx = c.getContext("2d");
+
+// clears the canvas
+var clearButton = document.getElementById('clear');
+clearButton.addEventListener("click", function(e) {
+  ctx.clearRect(0, 0, width, height);
+});
+
+// state variable, if true: rectangle, if false: dot
+var isRectangle = true;
+var toggleButton = document.getElementById('toggle');
+
+// toggle button switches between draw-a-rectangle mode and draw-a-dot mode
+toggleButton.addEventListener('click', function() {
+  isRectangle = !isRectangle;
+});
+
+c.addEventListener('click', function(e) {
+  // prevent the default action of clicking on canvas
+  e.preventDefault();
+  if(isRectangle) {
+    console.log("Rectangle");
+    ctx.fillStyle = '#FF0000'; // red
+    // get coordinates relative to element
+    ctx.fillRect(e.offsetX, e.offsetY, 50, 50);
+  }
+  else {
+    console.log("Ellipse");
+    ctx.fillStyle = '#00FF00'; // green
+    // starts the path of the ellipse all over again every time it is clicked
+    ctx.beginPath();
+    // get coordinates relative to element
+    ctx.ellipse(e.offsetX, e.offsetY, 50, 50, 0, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+});
