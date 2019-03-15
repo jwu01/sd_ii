f8fc349d68271fb7861656b4daaa6ae19ea9c79f
jwu01 <jwu29@stuy.edu>
2019-03-15 13:21:19 -0400

    11

---
+++ 11_svg/svg.js
@@ -0,0 +1,43 @@
+var box = document.getElementById("box");
+var clear = document.getElementById("clear");
+var newC = true;
+var move = document.getElementById("move");
+
+box.addEventListener('click', function(e){
+    e.preventDefault
+    x = e.offsetX;
+    y = e.offsetY;
+
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.addEventListener('click', (e) => {
+            console.log(circle)
+            if (circle.getAttribute('fill') == 'red') {
+                circle.setAttribute('fill', 'blue');
+            } 
+            else {
+                circle.setAttribute('fill','red');
+                circle.setAttribute("cx", Math.floor(Math.random() * 501));
+                circle.setAttribute("cy", Math.floor(Math.random() * 501));
+            }
+            newC = false;
+    });
+
+    if(newC){  
+        circle.setAttribute("cx", x);
+        circle.setAttribute("cy", y);
+        circle.setAttribute("r", "20");
+        circle.setAttribute("stroke", "black");
+        circle.setAttribute("fill", "red");
+        box.appendChild(circle)
+    }
+
+    newC = true;
+})
+
+move.addEventListener('click', function(){
+}
+clear.addEventListener('click', function(){
+	while (box.firstChild) {
+		box.removeChild(box.firstChild);
+    }
+})
